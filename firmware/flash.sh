a11ab8861544a9b4a1524f664a571cc1b3a83a8c
mars <martin@rebel-it.com>
2016-09-16 12:29:41 +0100

    reverted to general version

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dfu-util-0.8-binaries/win32-mingw32/dfu-util.exe -d 2b04:d006 -a 0 -s 0x8020000:leave -D build/target/main/platform-6/opensound.bin
+DFUUTIL=dfu-util
+$DFUUTIL -d 2b04:d006 -a 0 -s 0x8020000:leave -D build/target/main/platform-6/opensound.bin
